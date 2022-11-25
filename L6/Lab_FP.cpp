b4d4bf19b4cdb0c526a56cce6fd1b3e3ad544e37
krzychu-bk <115548542+krzychu-bk@users.noreply.github.com>
2022-11-25 10:13:51 +0100

    changed var names to avoid antiplagiarism system

--- L6/Lab_FP.cpp
+++ L6/Lab_FP.cpp
@@ -1,55 +1,55 @@
 #include <iostream>
 using namespace std;
 
-#define MSIZE 10
+#define MAX_SIZE 10
 
-void input(char array[MSIZE][MSIZE], int size[2], int* mode, int* arg) {
+void input(char source[MAX_SIZE][MAX_SIZE], int size[2], int* mode, int* turns) {
 	for (int i = 0; i < 2; i++) {
 		cin >> size[i];
 	}
 	for (int j = 0; j < size[0]; j++) {
 		for (int k = 0; k < size[1]; k++) {
-			cin >> array[j][k];
+			cin >> source[j][k];
 		}
 	}
 	cin >> *mode;
 	if (*mode == 1) {
-		cin >> *arg;
+		cin >> *turns;
 	}
 }
 
-void mode0(char array[MSIZE][MSIZE],int nofN[MSIZE][MSIZE], int size[2]) {
+void countNeighbours(char source[MAX_SIZE][MAX_SIZE],int neiMap[MAX_SIZE][MAX_SIZE], int size[2]) {
 	for (int b = 0; b < size[0]; b++) {
 		for (int a = 0; a < size[1]; a++) {
-			nofN[b][a] = 0;
+			neiMap[b][a] = 0;
 		}
 	}
 	for (int i = 0; i < size[0]; i++) {
 		for (int j = 0; j < size[1]; j++) {
-			if (array[i][j]=='1') {
+			if (source[i][j]=='1') {
 				if (i != 0) {
 					if (j != 0) {
-						nofN[i - 1][j - 1]++;
+						neiMap[i - 1][j - 1]++;
 					}
-					nofN[i - 1][j]++;
-					nofN[i - 1][j + 1]++;
+					neiMap[i - 1][j]++;
+					neiMap[i - 1][j + 1]++;
 				}
 				if (j != 0) {
-					nofN[i][j - 1]++;
-					nofN[i + 1][j - 1]++;
+					neiMap[i][j - 1]++;
+					neiMap[i + 1][j - 1]++;
 				}		
-				nofN[i][j + 1]++;
-				nofN[i + 1][j]++;
-				nofN[i + 1][j + 1]++;
+				neiMap[i][j + 1]++;
+				neiMap[i + 1][j]++;
+				neiMap[i + 1][j + 1]++;
 			}
 		}
 	}
 }
 
-void printC(char array[MSIZE][MSIZE], int size[2]) {
+void charPrint(char source[MAX_SIZE][MAX_SIZE], int size[2]) {
 	for (int y = 0; y < size[0]; y++) {
 		for (int x = 0; x < size[1]; x++) {
-			cout << array[y][x];
+			cout << source[y][x];
 			if (x == (size[1] - 1)) {
 				cout << endl;
 			}
@@ -57,10 +57,10 @@ void printC(char array[MSIZE][MSIZE], int size[2]) {
 	}
 }
 
-void printI(int array[MSIZE][MSIZE], int size[2]) {
+void intPrint(int source[MAX_SIZE][MAX_SIZE], int size[2]) {
 	for (int y = 0; y < size[0]; y++) {
 		for (int x = 0; x < size[1]; x++) {
-			cout << array[y][x];
+			cout << source[y][x];
 			if (x == (size[1] - 1)) {
 				cout << endl;
 			}
@@ -68,51 +68,51 @@ void printI(int array[MSIZE][MSIZE], int size[2]) {
 	}
 }
 
-void mode1(char array[MSIZE][MSIZE], int nofN[MSIZE][MSIZE], int size[2], int arg) {
-	char dest[MSIZE][MSIZE];
-	printC(array, size);
+void game(char source[MAX_SIZE][MAX_SIZE], int neiMap[MAX_SIZE][MAX_SIZE], int size[2], int turns) {
+	char dest[MAX_SIZE][MAX_SIZE];
+	charPrint(source, size);
 	cout << endl;
-	for (int a = 0; a < arg; a++) {
-		mode0(array, nofN, size);
+	for (int a = 0; a < turns; a++) {
+		countNeighbours(source, neiMap, size);
 		for (int i = 0; i < size[0]; i++) {
 			for (int j = 0; j < size[1]; j++) {
-				if (array[i][j] == '0') {
-					if (nofN[i][j] == 3) {
+				if (source[i][j] == '0') {
+					if (neiMap[i][j] == 3) {
 						dest[i][j] = '1';
 					}
 					else dest[i][j] = '0';
 				}
-				if (array[i][j] == '1') {
-					if ((nofN[i][j] == 2) || nofN[i][j] == 3) {
+				if (source[i][j] == '1') {
+					if ((neiMap[i][j] == 2) || neiMap[i][j] == 3) {
 						dest[i][j] = '1';
 					}
 					else dest[i][j] = '0';
 				}
-				if (array[i][j] == 'X') {
+				if (source[i][j] == 'X') {
 					dest[i][j] = 'X';
 				}
 			}
 		}
-		printC(dest, size);
+		charPrint(dest, size);
 		cout << endl;
 		for (int k = 0; k < size[0]; k++) {
 			for (int l = 0; l < size[1]; l++) {
-				array[k][l] = dest[k][l];
+				source[k][l] = dest[k][l];
 			}
 		}
 	}
 }
 
 int main() {
-	char board[MSIZE][MSIZE];
-	int size[2], mode, arg, nofN[MSIZE][MSIZE];
-	input(board, size, &mode, &arg);
+	char board[MAX_SIZE][MAX_SIZE];
+	int size[2], mode, turns, neiMap[MAX_SIZE][MAX_SIZE];
+	input(board, size, &mode, &turns);
 	if (mode == 0) {
-		mode0(board,nofN,size);
-		printI(nofN, size);
+		countNeighbours(board,neiMap,size);
+		intPrint(neiMap, size);
 	}
 	if (mode == 1) {
-		mode1(board, nofN,size,arg);
+		game(board, neiMap,size,turns);
 	}
 	return 0;
 }
